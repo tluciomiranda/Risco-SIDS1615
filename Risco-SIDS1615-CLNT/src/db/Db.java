fc709e01f7b7607cdb8b8f8a2c01383acb3fadfb
tluciomiranda <tluciomiranda@gmail.com>
2016-05-29 16:02:29 +0100

    divisao cliente servidor

---
+++ Risco-SIDS1615-CLNT/src/db/Db.java
@@ -0,0 +1,127 @@
+package db;
+import java.io.File;
+import java.sql.*;
+
+
+public class Db {
+	
+	public Db(){
+		
+	};
+	
+	
+	public void startDb(){
+		System.out.println("Connecting to local database");
+		File file = new File ("risk.db");
+
+		if(file.exists()){
+			System.out.println("This database name already exists");
+		}
+		else{
+			
+			System.out.println("Database doesn't exist");
+			
+			try {
+				Connection c = null;
+			    Statement stmt = null;
+				Class.forName("org.sqlite.JDBC");
+				c = DriverManager.getConnection("jdbc:sqlite:risk.db");
+				
+				stmt = c.createStatement();
+				
+				String sql = "CREATE TABLE USER " +
+		                   "(ROWID INTEGER PRIMARY KEY," +
+		                   " USERNAME TEXT UNIQUE NOT NULL, " + 
+		                   " USERNAMELOWER TEXT UNIQUE NOT NULL, " + 
+		                   " EMAIL TEXT UNIQUE NOT NULL, " + 
+		                   " PASSWORD TEXT NOT NULL, " +
+		                   "updatetime TEXT DEFAULT (strftime('%Y-%m-%d %H:%M:%S:%s','now', 'localtime')))"; 
+				stmt.executeUpdate(sql);
+			
+				System.out.println("Database table created");
+				
+				sql = "CREATE TRIGGER update_user  BEFORE update ON USER " + 
+								"begin " +
+								"update USER set updatetime = strftime('%Y-%m-%d %H:%M:%S:%s','now', 'localtime') where ROWID = old.ROWID;"+
+								"end";
+				stmt.executeUpdate(sql);
+				
+				System.out.println("Database trigger created");
+				
+				
+				stmt.close();
+				c.close();
+				
+				
+			} catch ( Exception e ) {
+				System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+				System.exit(0);
+			}
+			System.out.println("Opened database successfully");
+		}
+	}
+	
+	//SELECT * FROM USER WHERE updatedatetime > (SELECT updatedatetime FROM USER ORDER BY updatedatetime asc limit 1);
+	
+	public boolean registerUser(String username, String email, String password){
+		boolean value = false;
+		Connection c = null;
+	    Statement stmt = null;
+	    try {
+	      Class.forName("org.sqlite.JDBC");
+	      c = DriverManager.getConnection("jdbc:sqlite:risk.db");
+	      c.setAutoCommit(false);
+	      System.out.println("Opened database successfully");
+
+	      stmt = c.createStatement();
+	      String sql = "INSERT INTO USER (USERNAME, USERNAMELOWER,EMAIL,PASSWORD) " +
+	                   "VALUES ('"+ username +"' , '" + username.toLowerCase() +"' , '"+ email.toLowerCase() + "' , '" + password +"');"; 
+	      int rows = stmt.executeUpdate(sql);
+	      
+	      if(rows >0){
+	    	  value = true;
+	      }
+	      stmt.close();
+	      c.commit();
+	      c.close();
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	      System.exit(0);
+	    }
+	    return value;
+	}
+	
+	public boolean loginUser(String username, String password){
+		boolean value = false;
+		
+		Connection c = null;
+	    Statement stmt = null;
+	    try {
+	      Class.forName("org.sqlite.JDBC");
+	      c = DriverManager.getConnection("jdbc:sqlite:risk.db");
+	      c.setAutoCommit(false);
+	      System.out.println("Opened database successfully");
+
+	      stmt = c.createStatement();
+	      System.out.println("BEFORE");
+	      ResultSet rs = stmt.executeQuery( "SELECT COUNT(*) AS CNT FROM USER WHERE USERNAMELOWER='"+username.toLowerCase()+"'"+
+	    	      " AND PASSWORD='"+password+"';" );
+	      
+	      System.out.println("AFTER");
+	      if(rs.getInt("CNT")>0){
+	    	  value = true;
+	    	  System.out.println("Login Successful");
+	      }
+	      rs.close();
+	      stmt.close();
+	      c.close();
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	      System.exit(0);
+	    }
+	    System.out.println("Operation done successfully");
+		
+		return value;
+	}
+
+}
---
+++ Risco-SIDS1615-SVR/src/interfaces/Interfaces.java
@@ -0,0 +1,50 @@
+package interfaces;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Interfaces
+{
+	private static final int BUTTON_HEIGHT = 75;
+	private static final int BUTTON_WIDTH = 400;
+	private static final int TEXTFIELD_HEIGHT = 400;
+	private static final int TEXTFIELD_WIDTH = 50;
+	private static final int WINDOW_WIDTH = 1280;
+	private static final int WINDOW_HEIGHT = 720;
+	
+	// Login objects
+	
+	public static void main(String[] args) 
+	{
+		javax.swing.SwingUtilities.invokeLater(new Runnable()
+	    {
+	        public void run() {
+	        	createAndShowLoginUI();
+	        }
+	    });
+	}
+
+	private static void createAndShowLoginUI() 
+	{
+	    //Create and set up the window.
+	    JFrame frame = new JFrame("Risk");
+	    JPanel panel1 = new JPanel(new GridLayout(3,2));
+		JLabel welcomeLabel = new JLabel("Welcome to Risk Game.");
+		JLabel pleaseLoginLabel = new JLabel("Please login below, or create an account:");
+	    
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+	    
+	    panel1.add(welcomeLabel, BorderLayout.CENTER);
+	    panel1.add(pleaseLoginLabel, BorderLayout.CENTER);
+	    panel1.setSize(400, 400);
+	    frame.getContentPane().add(panel1, BorderLayout.CENTER);
+	    
+	    //Display the window.
+	    frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	    frame.setVisible(true);
+	}
+}
