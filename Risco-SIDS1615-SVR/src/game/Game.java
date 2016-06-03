8a20c7216b887335fc14b28cf3516ed387c7c107
Tiago Miranda <Tiago Miranda@192.168.0.155>
2016-06-03 02:18:53 +0100

    cenas2

--- Risco-SIDS1615-SVR/src/game/Game.java
+++ Risco-SIDS1615-SVR/src/game/Game.java
@@ -1,5 +1,8 @@
 package game;
 
+import java.io.IOException;
+
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public class Game implements HttpHandler 
@@ -7,5 +10,11 @@ public class Game implements HttpHandler
 	public Game()
 	{
 		//cenas
+	}
+
+	@Override
+	public void handle(HttpExchange e) throws IOException 
+	{
+		
 	}
 }
